17b64e7393331304a32ae7c4a842f5d312d5590c
soufiane odf <soufiane.odf@gmail.com>
2020-05-28 19:54:55 +0000

    Add spring security

---
+++ src/main/java/com/fst/asustech/security/config/DemoSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.fst.asustech.security.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource securityDataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").hasRole("ADMIN")
+			.and().formLogin().loginPage("/login")
+			.loginProcessingUrl("/authenticateTheUser").permitAll()
+			.and().logout().permitAll();
+//			.and().exceptionHandling().accessDeniedPage("/access-denied");
+	}
+
+}
