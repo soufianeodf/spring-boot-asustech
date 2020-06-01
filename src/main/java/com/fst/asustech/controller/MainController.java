75d364a43226ea20484f3af4e9713cabd6adbc39
soufiane odf <soufiane.odf@gmail.com>
2020-06-01 04:14:35 +0100

    Add authentication, error and basic pages

--- src/main/java/com/fst/asustech/controller/MainController.java
+++ src/main/java/com/fst/asustech/controller/MainController.java
@@ -1,43 +1,51 @@
 package com.fst.asustech.controller;
 
-import javax.faces.bean.ManagedBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.stereotype.Component;
+import com.fst.asustech.entity.g.stock.ProduitsApprovisionnement;
+import com.fst.asustech.entity.g.stock.ProduitsStock;
+import com.fst.asustech.entity.g.vente.Commandes;
+import com.fst.asustech.entity.g.vente.ProduitsPrix;
+import com.fst.asustech.entity.g.vente.Users;
+import com.fst.asustech.service.CrudService;
 
-@ManagedBean
-@Component
+@Controller
 public class MainController {
 
-//	// vente
-//	@Autowired
-//	@Qualifier("produitsPrixServiceImpl")
-//	private CrudService<ProduitsPrix> produitsPrixService;
-//
-//	@Autowired
-//	@Qualifier("commandesServiceImpl")
-//	private CrudService<Commandes> commandesService;
-//
-//	@Autowired
-//	@Qualifier("usersServiceImpl")
-//	private CrudService<Users> usersService;
-//
-//	// stock
-//	@Autowired
-//	@Qualifier("produitsStockServiceImpl")
-//	private CrudService<ProduitsStock> produitsStockService;
-//
-//	@Autowired
-//	@Qualifier("produitsApprovisionnementServiceImpl")
-//	private CrudService<ProduitsApprovisionnement> produitsApprovisionnementService;
-//
-//	@GetMapping("/")
-//	public String homePage(Model theModel) {
-//		return "dashboard/index";
-//	}
-//
-//	@GetMapping("/listProduits")
-//	public String listProduits(Model theModel) {
-//		theModel.addAttribute("produits", produitsPrixService.findAll());
-//		return "dashboard/pages/data-tables";
-//	}
+	// vente
+	@Autowired
+	@Qualifier("produitsPrixServiceImpl")
+	private CrudService<ProduitsPrix> produitsPrixService;
+
+	@Autowired
+	@Qualifier("commandesServiceImpl")
+	private CrudService<Commandes> commandesService;
+
+	@Autowired
+	@Qualifier("usersServiceImpl")
+	private CrudService<Users> usersService;
+
+	// stock
+	@Autowired
+	@Qualifier("produitsStockServiceImpl")
+	private CrudService<ProduitsStock> produitsStockService;
+
+	@Autowired
+	@Qualifier("produitsApprovisionnementServiceImpl")
+	private CrudService<ProduitsApprovisionnement> produitsApprovisionnementService;
+
+	@GetMapping("/")
+	public String homePage(Model theModel) {
+		return "/index.xhtml";
+	}
+
+	@GetMapping("/listProduits")
+	public String listProduits(Model theModel) {
+		theModel.addAttribute("produits", produitsPrixService.findAll());
+		return "/ecommerce-product.xhtml";
+	}
 }
--- src/main/java/com/fst/asustech/security/config/DemoSecurityConfig.java
+++ src/main/java/com/fst/asustech/security/config/DemoSecurityConfig.java
@@ -1,35 +1,36 @@
-//package com.fst.asustech.security.config;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@EnableWebSecurity
-//public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	private DataSource securityDataSource;
-//
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.jdbcAuthentication().dataSource(securityDataSource);
-//	}
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//			.antMatchers("/assets/**")
-//			.permitAll()
-//			.antMatchers("/**").hasRole("ADMIN")
-//			.and().formLogin().loginPage("/login")
-//			.loginProcessingUrl("/authenticateTheUser").permitAll()
-//			.defaultSuccessUrl("/", true)
-//			.and().logout().permitAll()
-//			.and().exceptionHandling().accessDeniedPage("/access-denied");
-//	}
-//
-//}
+package com.fst.asustech.security.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource securityDataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/assets/**")
+			.permitAll()
+			.antMatchers("/logout").hasAnyRole("EMPLOYEE")
+			.antMatchers("/**").hasRole("ADMIN")
+			.and().formLogin().loginPage("/login")
+			.loginProcessingUrl("/authenticateTheUser").permitAll()
+			.defaultSuccessUrl("/", true)
+			.and().logout().permitAll()
+			.and().exceptionHandling().accessDeniedPage("/access-denied");
+	}
+
+}
