0149093490fc87eeffa1057e99c5163c2bcd07eb
soufiane odf <soufiane.odf@gmail.com>
2020-06-04 23:03:23 +0100

    Uncomment WelcomePageRedirect to load index.xhtml in the root

--- src/main/java/com/fst/asustech/primefaces/WelcomePageRedirect.java
+++ src/main/java/com/fst/asustech/primefaces/WelcomePageRedirect.java
@@ -1,23 +1,23 @@
-//package com.fst.asustech.primefaces;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.Ordered;
-//import org.springframework.web.servlet.ViewResolver;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
-//
-//@Configuration
-//public class WelcomePageRedirect implements WebMvcConfigurer {
-//
-//  @Override
-//  public void addViewControllers(ViewControllerRegistry registry) {
-//    registry.addViewController("/")
-//        .setViewName("forward:/WEB-INF/index.xhtml");
-//    registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//  }
-//  
+package com.fst.asustech.primefaces;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WelcomePageRedirect implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/")
+        .setViewName("forward:/index.xhtml");
+    registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+  }
+  
 //	@Bean
 //	public ViewResolver getViewResolver(){
 //	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -25,4 +25,4 @@
 //	    resolver.setSuffix(".xhtml");
 //	    return resolver;
 //	}
-//}
+}
