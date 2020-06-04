bb8b8fadaff91593bd92b4b0675828392d164336
soufiane odf <soufiane.odf@gmail.com>
2020-06-04 22:26:36 +0100

    Add loadProduct method to bean and call it from the view

--- src/main/java/com/fst/asustech/controller/jsf/BeanProduit.java
+++ src/main/java/com/fst/asustech/controller/jsf/BeanProduit.java
@@ -2,14 +2,21 @@ package com.fst.asustech.controller.jsf;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fst.asustech.entity.g.stock.ProduitsStock;
 import com.fst.asustech.entity.g.vente.ProduitsPrix;
@@ -31,6 +38,7 @@ public class BeanProduit {
 	private CrudService<ProduitsStock> produitsStockService;
 
 	private List<ProduitsStock> produits;
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	public BeanProduit() {
 		produits = new ArrayList<ProduitsStock>();
@@ -65,4 +73,37 @@ public class BeanProduit {
 
 		return produits;
 	}
+
+	public String loadProduct(int codePdt) {
+
+		logger.info("loading student: " + codePdt);
+
+		try {
+			// get product from database
+			ProduitsPrix product = produitsPrixService.findById(codePdt);
+
+			// put in the request attribute ... so we can use it on the form page
+			ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+
+			Map<String, Object> requestMap = externalContext.getRequestMap();
+			requestMap.put("product", product);
+
+		} catch (Exception exc) {
+			// send this to server logs
+			logger.log(Level.SEVERE, "Error loading product id:" + codePdt, exc);
+
+			// add error message for JSF page
+			addErrorMessage(exc);
+
+			return null;
+		}
+
+		return "index";
+	}
+
+	private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
 }
